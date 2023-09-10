208e580abb2cd8348d081373021cf22d0c23d7a0
Javid Gasimov <javidgasimov98@gmail.com>
2023-09-10 15:58:14 +0400

    sidebar added

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -20,9 +20,36 @@ const getEntry = async (id: string) => {
 const EntryPage = async ({ params }: { params: { id: string } }) => {
   const entry = await getEntry(params.id);
 
+  const analysisData = [
+    { name: 'Summary', value: '' },
+    { name: 'Subject', value: '' },
+    { name: 'Mood', value: '' },
+    { name: 'Negative', value: false },
+  ];
+
   return (
-    <div className="w-full h-full">
-      <Editor entry={entry} />
+    <div className="w-full h-full grid grid-cols-3">
+      <div className="col-span-2">
+        <Editor entry={entry} />
+      </div>
+      <div className="border-l border-black/10">
+        <div className="bg-blue-300 px-6 py-10">
+          <h2 className="text-2xl">Analysis</h2>
+        </div>
+        <div>
+          <ul>
+            {analysisData.map((el) => (
+              <li
+                className="px-2 py-4 flex items-center justify-between border-b border-t border-black/10"
+                key={el.name}
+              >
+                <span className="text-lg font-semibold">{el.name}</span>
+                <span>{el.value}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
